f3a3d7986bea206972d947e04f3719c2d721cb5d
Minura Samaranayake <minura.samaranayake@gmail.com>
2025-05-26 01:42:40 -0500

    add files

---
+++ app/schemas/users.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    email: EmailStr
+    username: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    is_admin: bool
+
+    class Config:
+        orm_mode = True
